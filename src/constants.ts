e2ac689d804ac178be1597a57cd325a1fa0ed916
Alexandre Hebert <hebert.alex@gmail.com>
2025-04-12 18:21:11 -0400

    fixed compilation issues

--- src/constants.ts
+++ src/constants.ts
@@ -13,6 +13,7 @@ export const DEFAULT_ELO = 1000;
 export const DECAY_RATE = 5;
 export const DECAY_PERIOD_DAYS = 3;
 
+export const IS_GH_PAGES = import.meta.env.VITE_IS_GH_PAGES === 'true';
 export const BASE_URL = import.meta.env.VITE_BASE_URL || '.';
 export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || '/';
 export const GITHUB_REPOSITORY_DATA_FOLDER = import.meta.env.VITE_DATA_FOLDER || 'https://github.com/alexandrehebert/foosball-scores/edit/main/public';
